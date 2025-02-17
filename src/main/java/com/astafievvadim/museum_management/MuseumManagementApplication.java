0896c1966f83f0f419bdfb540c3b10c778dc4c54
ThatPotato <gamelikecar@gmail.com>
2025-02-17 16:54:57 +0400

    Init

---
+++ src/main/java/com/astafievvadim/museum_management/MuseumManagementApplication.java
@@ -0,0 +1,13 @@
+package com.astafievvadim.museum_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astafievvadim/museum_management/MuseumManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astafievvadim.museum_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
